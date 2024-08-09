e94737926538d85a4cb667f61c4e5e63abf9d182
MARWANANO32 <marwanano32@gmail.com>
2024-08-09 15:24:14 +0300

    MARWAN MOSTAFA

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,11 +2,13 @@
 -- id, integer, never null, auto increment and primary key
 -- email, string(255 characters), never null and unique
 -- name, string(255 characters)
+-- country, US, CO and TN, never null
 -- If table exists, script will not fail, can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users
 (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
